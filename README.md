f9c1fcbc2a056dd25baa2ca57e0479c5c9dda3e7
Phil Sinatra <code@philsinatra.com>
2017-10-13 13:35:43 -0400

    🎉  Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# html-nested-comments README
+
+**The problem**: if your HTML code contains a comment, and you want to add a new comment to temporarily disable a block or portion of code, the built in commenting functionality does not actually place the comment tags in expected locations. If an existing comment is included in the content being commented out, the first instance of a `-->` closing comment tag will end the entire comment.
+
+**The solution**: The extension very simply finds all comments within the highlighted block of code and converts dashes to tildes, and then places the comment tags in the expected locations. It also then reverses the effect to uncomment the same code.
+
+## Features
+
+If you need to comment out a portion of your code that includes pre-existing comments, the native commention functionality will not comment properly or preserve your existing comments. This extension will maintain your original comments and allow you to quickly toggle comments on sections of code.
+
+![code before nesting](images/demo.gif)
+
+## Extension Settings
+
+The following file formats are supported:
+
+- asp
+- cfm
+- htm
+- html
+- md
+- njk
+- php
+- svg
+- vue
+- xml
+
+## Known Issues
+
+None at this time 😃
+
+## Release Notes
+
+Full release notes are available in the CHANGELOG file.
---
+++ src/extension.ts
@@ -0,0 +1,94 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "html-nested-comments" is now active!'
+  );
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  let nestComments = new NestComments();
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'extension.nestComments',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      nestComments.updateNestedComments();
+      // Display a message box to the user
+      //   vscode.window.showInformationMessage('Hello World!');
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+class NestComments {
+  public updateNestedComments() {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    const doc = editor.document;
+    const supported = [
+      'asp',
+      'cfm',
+      'htm',
+      'html',
+      'md',
+      'njk',
+      'php',
+      'svg',
+      'vue',
+      'xml'
+    ];
+
+    if (supported.indexOf(doc.languageId) > -1) {
+      const selection = editor.selection;
+      const text = editor.document.getText(selection);
+
+      const prefix = text.substring(0, 4);
+      let mod_text = '';
+
+      if (prefix !== '<!--') {
+        mod_text = text.replace(/<!--/g, '<!~~');
+        mod_text = mod_text.replace(/-->/g, '~~>');
+        mod_text = '<!-- ' + mod_text + ' -->';
+      } else {
+        mod_text = text.replace(/<!-- /g, '');
+        mod_text = mod_text.replace(/<!--/g, '');
+        mod_text = mod_text.replace(/-->/g, '');
+        mod_text = mod_text.replace(/<!~~/g, '<!--');
+        mod_text = mod_text.replace(/~~>/g, '-->');
+      }
+
+      let edit = new vscode.WorkspaceEdit();
+
+      const startPos: vscode.Position = new vscode.Position(
+        selection.start.line,
+        selection.start.character
+      );
+      const endPos: vscode.Position = new vscode.Position(
+        selection.start.line + text.split(/\r\n|\r|\n/).length - 1,
+        selection.start.character + text.length
+      );
+
+      const range = new vscode.Range(startPos, endPos);
+
+      edit.replace(editor.document.uri, range, mod_text);
+      return vscode.workspace.applyEdit(edit);
+    } else {
+      vscode.window.showInformationMessage('File format not supported!');
+    }
+  }
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
